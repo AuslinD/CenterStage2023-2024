c38db313b81f64275e84bc9c0b18066801bbdaa2
Aashgarg <garg_aashna@icloud.com>
2023-09-19 16:07:27 -0500

    change number

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV.java
@@ -175,7 +175,7 @@ public class OpenCV extends LinearOpMode
         void inputToCb(Mat input)
         {
             Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(YCrCb, Cr, 2);
+            Core.extractChannel(YCrCb, Cr, 1);
         }
 
         @Override
