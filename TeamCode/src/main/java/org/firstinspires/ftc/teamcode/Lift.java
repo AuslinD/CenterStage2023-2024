9d50d1efba9bcb4abc6af54e82c0d5e8d392d82c
austindeng <austindeng3@gmail.com>
2023-09-26 16:00:30 -0500

    renamed stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -10,71 +10,71 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 public class Lift {
-    private DcMotor rn1,rn2,up1 ,up2 ; // rn stands for rotational motor
+    private DcMotor rotateLeft, rotateRight, liftLeft, liftRight; // rn stands for rotational motor
     public Lift(OpMode opMode){
-        rn1 = opMode.hardwareMap.get(DcMotorEx.class, "rn1");
-        rn2 = opMode.hardwareMap.get(DcMotorEx.class, "rn2");
-        up1 = opMode.hardwareMap.get(DcMotorEx.class, "up1");
-        up2 = opMode.hardwareMap.get(DcMotorEx.class, "up2");
+        rotateLeft = opMode.hardwareMap.get(DcMotorEx.class, "rotleft");
+        rotateRight = opMode.hardwareMap.get(DcMotorEx.class, "rotright");
+        liftLeft = opMode.hardwareMap.get(DcMotorEx.class, "upleft");
+        liftRight = opMode.hardwareMap.get(DcMotorEx.class, "upright");
 
-        rn1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rn2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        up1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        up2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotateLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotateRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        rn1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rn2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        up1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        up2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rotateLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rotateRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        rn1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rn2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        up1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        up2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotateLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotateRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        rn1.setDirection(DcMotorSimple.Direction.FORWARD);
-        rn2.setDirection(DcMotorSimple.Direction.FORWARD);
-        up1.setDirection(DcMotorSimple.Direction.FORWARD);
-        up2.setDirection(DcMotorSimple.Direction.FORWARD);
+        rotateLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        rotateRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        liftLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        liftRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
     }
 
     public Lift(LinearOpMode linearOpMode){
-        rn1 = linearOpMode.hardwareMap.get(DcMotorEx.class, "rn1");
-        rn2 = linearOpMode.hardwareMap.get(DcMotorEx.class, "rn2");
-        up1 = linearOpMode.hardwareMap.get(DcMotorEx.class, "up1");
-        up2 = linearOpMode.hardwareMap.get(DcMotorEx.class, "up2");
+        rotateLeft = linearOpMode.hardwareMap.get(DcMotorEx.class, "rotleft");
+        rotateRight = linearOpMode.hardwareMap.get(DcMotorEx.class, "rotright");
+        liftLeft = linearOpMode.hardwareMap.get(DcMotorEx.class, "upleft");
+        liftRight = linearOpMode.hardwareMap.get(DcMotorEx.class, "upright");
 
-        rn1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rn2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        up1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        up2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotateLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotateRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        rn1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rn2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        up1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        up2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rotateLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rotateRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        rn1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rn2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        up1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        up2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotateLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotateRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        rn1.setDirection(DcMotorSimple.Direction.FORWARD);
-        rn2.setDirection(DcMotorSimple.Direction.FORWARD);
-        up1.setDirection(DcMotorSimple.Direction.FORWARD);
-        up2.setDirection(DcMotorSimple.Direction.FORWARD);
+        rotateLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        rotateRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        liftLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        liftRight.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void setAllLiftMotorPower(double power){
-        rn1.setPower(power);
-        rn2.setPower(power);
-        up1.setPower(power);
-        up2.setPower(power);
+        rotateLeft.setPower(power);
+        rotateRight.setPower(power);
+        liftLeft.setPower(power);
+        liftRight.setPower(power);
     }
 
 
     public DcMotor[] getMotors(){
-        return new DcMotor[] {rn1, rn2, up1, up2};
+        return new DcMotor[] {rotateLeft, rotateRight, liftLeft, liftRight};
     }
 }
