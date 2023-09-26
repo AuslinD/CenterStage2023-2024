675e8d3dc50fbfcf5b37aa76dfb14fe83f5fcc84
Aashgarg <garg_aashna@icloud.com>
2023-09-26 16:57:11 -0500

    claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -6,14 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class Robot {
     Drivetrain drivetrain;
     Lift lift;
+    Claw claw;
     public Robot(OpMode opmode){
         drivetrain = new Drivetrain(opmode);
         lift = new Lift(opmode);
+        claw = new Claw();
     }
     public Robot(LinearOpMode linearOpMode){
         drivetrain = new Drivetrain(linearOpMode);
         lift = new Lift(linearOpMode);
-
+        claw = new Claw();
     }
 
 
