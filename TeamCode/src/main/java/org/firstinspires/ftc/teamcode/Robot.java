c18473304a1af7bad0073e5e5201fa76c966793d
austindeng <austindeng3@gmail.com>
2023-09-29 16:43:51 -0500

    field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,21 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
 
 public class Robot {
     Drivetrain drivetrain;
     Lift lift;
     Claw claw;
+
+    IMU imu;
+
+
     public Robot(OpMode opmode){
         drivetrain = new Drivetrain(opmode);
         lift = new Lift(opmode);
         claw = new Claw(opmode);
+
+        imu = opmode.hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
     }
     public Robot(LinearOpMode linearOpMode){
         drivetrain = new Drivetrain(linearOpMode);
         lift = new Lift(linearOpMode);
         claw = new Claw(linearOpMode);
+
+        imu = linearOpMode.hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+    }
+
+    public IMU getImu(){
+        return imu;
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMethods.java
@@ -2,74 +2,78 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class TeleOpMethods {
-    Robot robot;
+    private static Robot robot;
+    OpMode opMode;
     //Manipulator
 
     public TeleOpMethods(OpMode opMode)
     {   //manip later
         this.robot = new Robot(opMode);
+        this.opMode = opMode;
     }
 
     public void teleOpControls(Gamepad gamepad1, Gamepad gamepad2)
     {
-        double FLP = 0;
-        double FRP = 0;
-        double BRP = 0;
-        double BLP = 0;
+        driveTrainStuff(gamepad1, gamepad2);
+
+
+
+    }
+
+    private static void driveTrainStuff(Gamepad gamepad1, Gamepad gamepad2){
+        //Drivetrain Stuff
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+        IMU imu = robot.getImu();
+
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+
+        if(gamepad1.right_trigger > 0.1){
+            frontRightPower *= .5;
+            backRightPower *= .5;
+            frontLeftPower *= .5;
+            backLeftPower *= .5;
+        }
+        robot.drivetrain.fl.setPower(frontLeftPower);
+        robot.drivetrain.bl.setPower(backLeftPower);
+        robot.drivetrain.fr.setPower(frontRightPower);
+        robot.drivetrain.br.setPower(backRightPower);
+
+    }
+
+    private static void manipulatorStuff(Gamepad gamepad1, Gamepad gamepad2){
+
+        //Manipulator and lift stuff
         double rn1p = 0;
         double rn2p = 0;
         double up1p = 0;
         double up2p = 0;
-
-        if(Math.abs(gamepad1.left_stick_y) > 0.1 || Math.abs(gamepad1.left_stick_x) > 0.1 || Math.abs(gamepad1.right_stick_x) > 0.1) {
-            FLP += gamepad1.left_stick_y;
-            FRP += gamepad1.left_stick_y;
-            BRP += gamepad1.left_stick_y;
-            BLP += gamepad1.left_stick_y;
-
-            FLP += gamepad1.left_stick_x;
-            FRP += gamepad1.left_stick_x;
-            BRP += gamepad1.left_stick_x;
-            BLP += gamepad1.left_stick_x;
-
-            FLP += gamepad1.right_stick_x;
-            FRP += gamepad1.right_stick_x;
-            BRP += gamepad1.right_stick_x;
-            BLP += gamepad1.right_stick_x;
-
-            double max = Math.max(Math.max(Math.abs(FLP) , Math.abs(FRP)), Math.max((BRP), Math.abs(BLP)));
-
-            if (Math.abs(max) > 1) {
-                FRP /= Math.abs(max);
-                FLP /= Math.abs(max);
-                BRP /= Math.abs(max);
-                BLP /= Math.abs(max);
-            }
-            if(Math.abs(gamepad1.right_trigger) > 0.1) {
-
-                robot.drivetrain.getMotors()[0].setPower(FLP * .4);
-                robot.drivetrain.getMotors()[1].setPower(FRP * .4);
-                robot.drivetrain.getMotors()[2].setPower(BLP * .4);
-                robot.drivetrain.getMotors()[3].setPower(BRP * .4);
-            }
-            else {
-                robot.drivetrain.getMotors()[0].setPower(FLP);
-                robot.drivetrain.getMotors()[1].setPower(FRP);
-                robot.drivetrain.getMotors()[2].setPower(BLP);
-                robot.drivetrain.getMotors()[3].setPower(BRP);
-            }
-
-        }
-
-
-        else {
-            robot.drivetrain.getMotors()[0].setPower(0);
-            robot.drivetrain.getMotors()[1].setPower(0);
-            robot.drivetrain.getMotors()[2].setPower(0);
-            robot.drivetrain.getMotors()[3].setPower(0);
-        }
 
         if(Math.abs(gamepad2.left_stick_y) > 0.1 || Math.abs(gamepad2.left_stick_x) > 0.1 || Math.abs(gamepad2.right_stick_x) > 0.1) {
             up1p += gamepad2.left_stick_y;
@@ -106,10 +110,9 @@ public class TeleOpMethods {
         }
         if(gamepad2.b){
             robot.claw.clawUp();
-            
+
 
         }
-        if gamepad2.right_stick_y
     }
 
 
