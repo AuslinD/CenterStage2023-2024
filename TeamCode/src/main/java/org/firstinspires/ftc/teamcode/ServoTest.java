0c329a33bcd3287b0cbc4307304c9014060fb05d
Aashgarg <garg_aashna@icloud.com>
2023-10-28 18:39:31 -0500

    servo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -7,17 +7,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name = "ServoTest", group = "test")
 public class ServoTest extends LinearOpMode {
-    CRServo intakeLeft, intakeRight;
+    CRServo intakeLeft, intakeRight, transferLeft, transferRight;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
         intakeLeft = hardwareMap.get(CRServo.class, "inL");
         intakeRight = hardwareMap.get(CRServo.class, "inR");
+        transferLeft = hardwareMap.get(CRServo.class, "trL");
+        transferRight = hardwareMap.get(CRServo.class, "trR");
 
         waitForStart();
 
         intakeLeft.setPower(.5);
         intakeRight.setPower(.5);
+        transferRight.setPower(.5);
+        transferLeft.setPower(.5);
     }
 }
