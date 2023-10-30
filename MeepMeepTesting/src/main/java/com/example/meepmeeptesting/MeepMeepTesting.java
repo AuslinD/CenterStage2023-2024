86a615e9b75d109ad75bced2b1e35db35547f472
emily-012 <106678136+emily-012@users.noreply.github.com>
2023-10-30 16:29:48 -0500

    meep meeeeeeeeeeeeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -32,12 +32,35 @@ public class MeepMeepTesting {
                                 .splineTo(new Vector2d(58, 12), Math.toRadians(0))
                                 .build()
                 );
+        RoadRunnerBotEntity blueLeft = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(12, 60, Math.toRadians(-90)))
+                                .splineTo(new Vector2d(12, 36), Math.toRadians(-90))
+                                .setReversed(true)
+                                .splineTo(new Vector2d(58, 60), Math.toRadians(0))
+                                .build()
+                );
+        RoadRunnerBotEntity redLeft = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, -60, Math.toRadians(90)))
+                                .forward(30)
+                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+                                .build()
+                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(redRight)
+                .addEntity(redLeft)
+                .addEntity(blueLeft)
                 .addEntity(blueRight)
+
                 .start();
     }
 }
