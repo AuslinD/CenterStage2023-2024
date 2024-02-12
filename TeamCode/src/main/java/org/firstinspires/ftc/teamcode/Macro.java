dc1c71de561a43279561b83c61eb28d88b906345
MatthewRichman <61804141+Sirllamaa@users.noreply.github.com>
2024-02-12 10:44:35 -0600

    I think the reason the other threading one didnt work was becuase the timer wasnt on the same thread so maybe this with blocking code will work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macro.java
@@ -1,17 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Config
 public class Macro {
     static OpMode opMode;
-
+    
     public static Timer timer = new Timer();
-
+    
     private static Robot robot;
-    private static Claw outtake;
     private static Lift lift;
     
     public static boolean macro_run = false;
@@ -21,14 +18,10 @@ public class Macro {
         lift = robot.lift;
     }
     
-    public static double[] macro_timing = {0,0.5,1,1.5,2};
-
-
+    public static double[] macro_timing = {0, 0.5, 1, 1.5, 2};
+    
     public static void liftAngleToPos(int desiredPosition) {
         double tolerance = 100;
-        ElapsedTime liftAngleToPosTimer = new ElapsedTime();
-        liftAngleToPosTimer.reset();
-
         while (!(Math.abs(lift.rotateRight.getCurrentPosition() - desiredPosition) <= tolerance)) {
             int currentPos = lift.rotateRight.getCurrentPosition();
             if (Math.abs(currentPos - desiredPosition) <= tolerance) {
@@ -42,65 +35,51 @@ public class Macro {
         }
         lift.rotateRight.setPower(0);
     }
-
-
-    public static void macro_run(OpMode opmode){
+    
+    public static void macro_run(OpMode opmode) {
         timer.reset();
         
-        //Sets the starting pos of all the apendeges
         robot.claw.setClawPosition(0);
         robot.claw.setClawAngle(0.1);
-        liftAngleToPos (1000);
-        robot.lift.liftLeft.setTargetPosition(0);
-        robot.lift.liftRight.setTargetPosition(0);
-        boolean[] macro_state = {false,false,false,false,false};
-
-        //Start of the actual macro
-        if (lift.rotateRight.getCurrentPosition() + 5 > 200 && 200 < lift.rotateRight.getCurrentPosition() - 5) {
-            macro_run = true;
-        }
-        macro_state[0] = true;
-        while (macro_run){
-            liftAngleToPos (73);
-
-            if (timer.hasElapsed (macro_timing[0]) && macro_state[0]) {
-                robot.claw.setClawPosition (0.3);
-
-                macro_state[0] = false;
-                macro_state[1] = true;
-            }
-            if (timer.hasElapsed (macro_timing[1]) && macro_state[1]) {
+        liftAngleToPos(1000);
+        
+        macro_run = true;
+        
+        Thread macroThread = new Thread(() -> {
+            while (macro_run) {
+                liftAngleToPos(73);
+                waitUntilTimeElapsed(macro_timing[0]);
+                
+                robot.claw.setClawPosition(0.3);
+                waitUntilTimeElapsed(macro_timing[1]);
+                
                 robot.lift.liftLeft.setTargetPosition(85);
                 robot.lift.liftRight.setTargetPosition(85);
                 liftAngleToPos(100);
-                robot.claw.setClawAngle (0.115);
+                robot.claw.setClawAngle(0.115);
                 robot.claw.setClawPosition(0.00);
-
-                macro_state[1] = false;
-                macro_state[2] = true;
-            }
-            if (timer.hasElapsed (macro_timing[2]) && macro_state[2]) {
-                liftAngleToPos (60);
-                robot.claw.setClawPosition (0.8);
-
-                macro_state[2] = false;
-                macro_state[3] = true;
-            }
-            if (timer.hasElapsed (macro_timing[3]) && macro_state[3]) {
-                robot.claw.setClawPosition (0.5);
-                robot.claw.setClawAngle (0.1);
-
-                macro_state[3] = false;
-                macro_state[4] = true;
-            }
-            if (timer.hasElapsed (macro_timing[4]) && macro_state[4]) {
-                liftAngleToPos (800);
-                robot.claw.setClawAngle (0.41);
-
-                macro_state[4] = false;
+                waitUntilTimeElapsed(macro_timing[2]);
+                
+                liftAngleToPos(60);
+                robot.claw.setClawPosition(0.8);
+                waitUntilTimeElapsed(macro_timing[3]);
+                
+                robot.claw.setClawPosition(0.5);
+                robot.claw.setClawAngle(0.1);
+                waitUntilTimeElapsed(macro_timing[4]);
+                
+                liftAngleToPos(800);
+                robot.claw.setClawAngle(0.41);
                 macro_run = false;
-
             }
+        });
+        
+        macroThread.start();
+    }
+    
+    private static void waitUntilTimeElapsed(double targetTime) {
+        while (true) {
+            if (!(timer.hasElapsed (targetTime))) break;
         }
     }
 }
